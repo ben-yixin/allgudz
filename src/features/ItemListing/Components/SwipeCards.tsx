9a0a81314e7ce8bab98e8c81e0a5af7d2e899ed7
Ric <ricjohn.genoguin@gmail.com>
2024-04-06 11:05:50 +1300

    refactor: use shadow library for neubrutalism look

--- src/features/ItemListing/Components/SwipeCards.tsx
+++ src/features/ItemListing/Components/SwipeCards.tsx
@@ -3,6 +3,7 @@ import {View, StyleSheet, Text, ImageBackground} from "react-native";
 import Swiper from 'react-native-deck-swiper'
 import {placeholderImages} from "../../../assets";
 import {LinearGradient} from "expo-linear-gradient";
+import {Shadow} from "react-native-shadow-2";
 
 export const SwipeCards = () => {
 
@@ -34,20 +35,25 @@ export const SwipeCards = () => {
                 cards={mockData}
                 renderCard={(mockData: any) => {
                     return (
-                        <View style={styles.card}>
-
-                            <ImageBackground style={styles.imageBackground}
-                                             imageStyle={styles.image}
-                                             source={mockData.imageSrc}>
-                                <LinearGradient style={styles.gradient} colors={['transparent','rgba(0,0,0,0.9)']}>
-                                    <View style={styles.cardDetails}>
-                                        <Text style={styles.name}>{mockData.name}</Text>
-                                        <Text style={styles.description}>{mockData.description}</Text>
-                                    </View>
-                                </LinearGradient>
-
-                            </ImageBackground>
-                        </View>
+                        <Shadow style={{height: '100%', width: '100%'}}
+                                distance={1}
+                                offset={[20, 20]}
+                                startColor={"#000000ff"}
+                                endColor={"#00000000"}
+                        >
+                            <View style={styles.card}>
+                                <ImageBackground style={styles.imageBackground}
+                                                 imageStyle={styles.image}
+                                                 source={mockData.imageSrc}>
+                                    <LinearGradient style={styles.gradient} colors={['transparent', 'rgba(0,0,0,0.9)']}>
+                                        <View style={styles.cardDetails}>
+                                            <Text style={styles.name}>{mockData.name}</Text>
+                                            <Text style={styles.description}>{mockData.description}</Text>
+                                        </View>
+                                    </LinearGradient>
+                                </ImageBackground>
+                            </View>
+                        </Shadow>
                     )
                 }}
                 onSwiped={(cardIndex) => {
@@ -65,19 +71,15 @@ export const SwipeCards = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-    },
+    container: {},
     gradient: {
         height: '100%',
         borderRadius: 20,
     },
     card: {
-        flex: 1,
+        height: '96%',
+        width: '96%',
         borderRadius: 20,
-        borderWidth: 2,
-        borderRightWidth: 10,
-        borderColor: "black",
-        justifyContent: "center",
         backgroundColor: 'white',
     },
     cardDetails: {
@@ -86,14 +88,7 @@ const styles = StyleSheet.create({
         bottom: 40
     },
     imageBackground: {
-        flex: 1,
-        // shadowOffset: {
-        //     width: -100,
-        // },
-        // shadowRadius: 100,
-        borderTopLeftRadius: 20,
-        borderBottomLeftRadius: 20,
-        backgroundColor: 'black'
+        borderRadius: 20,
     },
     image: {
         borderRadius: 20
